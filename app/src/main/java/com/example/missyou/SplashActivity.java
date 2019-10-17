212dab273865f2123a38b0bfb77a05e2f56898d5
Hye Seo Shin <janeshin059@gmail.com>
2019-10-17 14:51:13 +0800

    SplashActivity
    added SplashActivity

---
+++ app/src/main/java/com/example/missyou/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.missyou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(3000);
+
+        }catch(InterruptedException e){
+
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,LoginActivity.class));
+        finish();
+
+
+
+    }
+
+
+
+}
